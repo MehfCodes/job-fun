5d821d149d2b07578269e783cc6634c33ea1cabe
erfanCodes <emailkarierfan@gmail.com>
2020-05-08 19:50:41 +0430

    make alive factory.js and refactor hiringAdController.js

--- controllers/factory.js
+++ controllers/factory.js
@@ -1,73 +1,84 @@
-// const { ObjectId } = require('mongoose').Types;
-// const catchAsync = require('./../utils/catchAsync');
-// const AppError = require('./../utils/appError');
+const { ObjectId } = require('mongoose').Types;
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+const APIFeatures = require('./../utils/apiFeatures');
+function getAll(Model) {
+  return catchAsync(async (req, res, next) => {
+    let data;
+    if (req.query) {
+      const features = new APIFeatures(Model.find(), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .pagination();
 
-// function getAll(model) {
-//   return catchAsync(async (req, res, next) => {
-//     const data = await model.find();
-//     res.status(200).json({
-//       status: 'success',
-//       results: data.length,
-//       data: {
-//         data
-//       }
-//     });
-//   });
-// }
+      data = await features.query;
+    } else {
+      data = await Model.find();
+    }
+    res.status(200).json({
+      status: 'success',
+      results: data.length,
+      data: {
+        data
+      }
+    });
+  });
+}
 
-// function getOne(model, dataType) {
-//   return catchAsync(async (req, res, next) => {
-//     if (!ObjectId.isValid(req.params.id))
-//       return next(new AppError(`user not found`, 404));
-//     const data = await model.findById(req.params.id);
-//     if (!data) return next(new AppError(`${dataType}not found`, 404));
-//     res.status(200).json({
-//       status: 'success',
-//       data: {
-//         data
-//       }
-//     });
-//   });
-// }
+function getOne(Model, dataType = '') {
+  return catchAsync(async (req, res, next) => {
+    if (!ObjectId.isValid(req.params.id))
+      return next(new AppError('id is not valid', 401));
+    const data = await Model.findById(req.params.id);
+    if (!data) return next(new AppError(`${dataType}not found`, 404));
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data
+      }
+    });
+  });
+}
 
-// function updateOne(model, dataType) {
-//   return catchAsync(async (req, res, next) => {
-//     const data = await model.findByIdAndUpdate(req.params.id, req.body, {
-//       new: true,
-//       runValidators: true
-//     });
+function updateOne(Model, dataType = '') {
+  return catchAsync(async (req, res, next) => {
+    const data = await Model.findByIdAndUpdate(req.user.id, req.body, {
+      new: true,
+      runValidators: true
+    });
 
-//     if (!data) return next(new AppError(`${dataType}not found`, 404));
-//     res.status(200).json({
-//       status: 'success',
-//       data: {
-//         data
-//       }
-//     });
-//   });
-// }
+    if (!data) return next(new AppError(`${dataType}not found`, 404));
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data
+      }
+    });
+  });
+}
 
-// function createOne(model) {
-//   return catchAsync(async (req, res, next) => {
-//     const data = await model.create(req.body);
-//     res.status(200).json({
-//       status: 'success',
-//       data: {
-//         data
-//       }
-//     });
-//   });
-// }
+function createOne(Model) {
+  return catchAsync(async (req, res, next) => {
+    const data = await Model.create(req.body);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data
+      }
+    });
+  });
+}
 
-// function deleteOne(model) {
-//   return catchAsync(async (req, res, next) => {
-//     const data = await model.findByIdAndDelete(req.params.id);
-//     res.status(200).json({
-//       status: 'success',
-//       data: {
-//         data
-//       }
-//     });
-//   });
-// }
-// module.exports = { getAll, getOne, updateOne, createOne, deleteOne };
+function deleteOne(Model) {
+  return catchAsync(async (req, res, next) => {
+    const data = await Model.findByIdAndDelete(req.user.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data
+      }
+    });
+  });
+}
+module.exports = { getAll, getOne, updateOne, createOne, deleteOne };
--- controllers/hiringAdController.js
+++ controllers/hiringAdController.js
@@ -6,91 +6,15 @@ const {
   createOne,
   deleteOne
 } = require('./factory');
-const { ObjectId } = require('mongoose').Types;
-const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
-const allhiringAd = catchAsync(async (req, res, next) => {
-  if (req.query) {
-    const features = new APIFeatures(HiringAd.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .pagination();
 
-    const ads = await features.query;
-    res.status(200).json({
-      status: 'success',
-      results: data.length,
-      data: {
-        ads
-      }
-    });
-  } else {
-    const ads = await HiringAd.find();
-    res.status(200).json({
-      status: 'success',
-      results: data.length,
-      data: {
-        ads
-      }
-    });
-  }
-});
-
-const getHiringAd = catchAsync(async (req, res, next) => {
-  if (!ObjectId.isValid(req.params.id))
-    return next(new AppError(`data not found`, 404));
-  const data = await HiringAd.findById(req.params.id);
-  if (!data) return next(new AppError(`data not found`, 404));
-  res.status(200).json({
-    status: 'success',
-    data: {
-      data
-    }
-  });
-});
+const allhiringAd = getAll(HiringAd);
+const getHiringAd = getOne(HiringAd, 'document');
 
 // only company can create update and delete hiring ads
-const createHiringAd = catchAsync(async (req, res, next) => {
-  if (req.user) {
-    const data = await HiringAd.create(req.body);
-    res.status(200).json({
-      status: 'success',
-      data: {
-        data
-      }
-    });
-  } else {
-    return next(
-      new AppError('you have not pernision to access this route', 403)
-    );
-  }
-});
-
-const updateHiringAd = catchAsync(async (req, res, next) => {
-  const data = await HiringAd.findByIdAndUpdate(req.user.id, req.body, {
-    new: true,
-    runValidators: true
-  });
-
-  if (!data) return next(new AppError(`data not found`, 404));
-  res.status(200).json({
-    status: 'success',
-    data: {
-      data
-    }
-  });
-});
-
-const deleteHiringAd = catchAsync(async (req, res, next) => {
-  const data = await HiringAd.findByIdAndDelete(req.user.id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      data
-    }
-  });
-});
+const createHiringAd = createOne(HiringAd);
+const updateHiringAd = updateOne(HiringAd, 'document');
+const deleteHiringAd = deleteOne(HiringAd);
 
 module.exports = {
   allhiringAd,
